c4ea4e10645c8410f947b939db814df8cc58a024
Denis Bizhanov <bizdenis3@gmail.com>
2021-03-12 23:13:16 +0100

    commit 12.03 23:10

---
+++ Networks.java
@@ -0,0 +1,7 @@
+public class Networks {
+    public static void main(String[] args) {
+        // Будет с ошибкой, в какой-то момент заблокируется
+        new Thread(() -> new Server().start()).start();
+        new Thread(() -> new Client().start()).start();
+    }
+}
